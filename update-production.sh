c9895be7e4ee6bd4ebe29096f01561206c10bf97
Vortechstudio <contact@vortechstudio.fr>
2024-05-08 11:16:09 +0200

    ci: Ajout de scripts de déploiement et workflow GitHub
    - Création du fichier de workflow .github/workflows/deploy.yml pour le déploiement continu
    - Ajout de scripts bash pour mise à jour en staging et en production

---
+++ update-production.sh
@@ -0,0 +1,19 @@
+#\bin\bash
+
+php artisan down
+
+git reset --hard
+git pull origin production
+git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+
+composer install --prefer-dist --no-interaction
+npm install
+
+php artisan cache:clear
+php artisan route:clear
+php artisan config:clear
+php artisan view:clear
+php artisan clear
+
+php artisan up
+
---
+++ update-staging.sh
@@ -0,0 +1,18 @@
+#\bin\bash
+
+php artisan down
+
+git reset --hard
+git pull origin master
+
+composer install --prefer-dist --no-interaction
+npm install
+
+php artisan cache:clear
+php artisan route:clear
+php artisan config:clear
+php artisan view:clear
+php artisan clear
+
+php artisan up
+
