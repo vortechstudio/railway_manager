3f5f14a59a877731ce769c2f75c675f22e23339f
Vortechstudio <contact@vortechstudio.fr>
2024-05-12 11:46:54 +0200

    feat(Tableau de Bord): Ajout d'un nouveau widget et de sa vue associée pour le système de quête (agenda)
    - Création de DashboardAgenda sous Livewire/Widget
    - Ajout de la vue dashboard-agenda sous resources/views/livewire/widget

---
+++ app/Livewire/Widget/DashboardAgenda.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Widget;
+
+use Livewire\Component;
+
+class DashboardAgenda extends Component
+{
+    public function render()
+    {
+        return view('livewire.widget.dashboard-agenda');
+    }
+}
---
+++ resources/views/livewire/widget/dashboard-agenda.blade.php
@@ -0,0 +1,11 @@
+<div class="card shadow-sm">
+    <div class="card-header">
+        <div class="p-3">
+            <h3 class="card-title">Agenda</h3>
+            <span class="card-subtitle text-muted">Réalisez les objectifs de l'Agenda et gagnez des ressources</span>
+        </div>
+    </div>
+    <div class="card-body">
+        Lorem Ipsum is simply dummy text...
+    </div>
+</div>
