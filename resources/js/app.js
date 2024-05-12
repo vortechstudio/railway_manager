9401174468f8e0ae06ed20a2f5f4586d28de9ed3
Vortechstudio <contact@vortechstudio.fr>
2024-05-12 11:45:43 +0200

    chore(app.js, package.json): Ajout de Swiper et mise à jour des dépendances
    - Ajout de la bibliothèque Swiper dans app.js et configuration basique
    - Ajout de Swiper dans les dépendances du package.json
    - Mise à jour de la liste des dépendances de développement du package.json

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 import './bootstrap';
+import Swiper from 'swiper';
+import 'swiper/css'
+
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 30,
+    hashNavigation: {
+        watchState: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
