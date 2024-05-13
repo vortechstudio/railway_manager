b45014644245ad2d0c65a5be67035cbda88cd545
Vortechstudio <contact@vortechstudio.fr>
2024-05-13 19:33:23 +0200

    chore(Core): Ajouter la gestion de réputation pour les utilisateurs
    - Intégration d'un système de réputation pour les utilisateurs
    - Création d'une migration pour ajouter un champ "reputation" à la table user_railways
    - Ajout de méthodes pour l'ajout de réputation en fonction de différents types d'action (engine, hubs, ligne, quest)
    - Ajout d'une notification d'augmentation de réputation
    - Ajout de la bibliothèque de gestion des push notifications au modèle User.

---
+++ database/migrations/2024_05_13_154928_add_reputation_to_user_railway.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('user_railways', function (Blueprint $table) {
+            $table->bigInteger('reputation')->default(0);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('user_railways', function (Blueprint $table) {
+            $table->dropColumn('reputation');
+        });
+    }
+};
