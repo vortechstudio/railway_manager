fa2afc8261450a01a9a229008aca3baa35608399
Vortechstudio <contact@vortechstudio.fr>
2024-05-13 13:58:57 +0200

    style(Profil): Ajout de l'animation de fond et de l'onglet profil utilisateur
    - Ajout d'une animation de fond pour l'ID 'kt_app_content'
    - Ajout de l'onglet profil utilisateur avec Livewire à la vue profil utilisateur.

--- resources/views/account/profil.blade.php
+++ resources/views/account/profil.blade.php
@@ -10,12 +10,15 @@
 @endsection
 
 @section("content")
-    <div id="kt_app_content" class="app-content">
+    <div id="kt_app_content" class="app-content animated-background h-100">
         <div class="card shadow-sm bg-brown-800">
             <div class="card-body">
                 <div class="row">
                     <div class="col-sm-12 col-lg-5 mb-5">
                         <livewire:account.profil-card-user :user="$user" />
+                    </div>
+                    <div class="col-sm-12 col-lg-7 mb-5">
+                        <livewire:account.profil-tab :user="$user" />
                     </div>
                 </div>
             </div>
