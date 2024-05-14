376f7baf2bb356678165e5bfaa93019c7378546a
Vortechstudio <contact@vortechstudio.fr>
2024-05-14 12:25:45 +0200

    feat(Messagerie): Ajout de fonctionnalités et de vues pour la gestion des boîtes mail
    - Création du composant Livewire `Mailboxes` avec des méthodes pour lire, réclamer des récompenses et supprimer tous les messages
    - Ajout de la vue associée pour afficher les messages dans les boîtes mail
    - Création d'un composant `ModalReward` pour afficher une récompense dans une fenêtre modale
    - Ajout de la vue associée pour ce composant modale.

---
+++ app/Livewire/Core/ModalReward.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Core;
+
+use Livewire\Component;
+
+class ModalReward extends Component
+{
+    public function render()
+    {
+        return view('livewire.core.modal-reward');
+    }
+}
