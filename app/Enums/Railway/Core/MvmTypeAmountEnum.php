c867e1a66973170251a23322c4f0c527a8231da0
Vortechstudio <contact@vortechstudio.fr>
2024-05-16 01:24:45 +0200

    feat(General):Ajout de modèles et migrations pour la gestion des mouvements de compagnies ferroviaires
    - Création des modèles UserRailwayCompany et UserRailwayMouvement.
    - Création des migrations pour ces modèles : "create_user_railway_companies_table" et "create_user_railway_mouvements_table".
    - Définition des types d'énumérations pour les mouvements : "MvmTypeMvmEnum" et "MvmTypeAmountEnum".

---
+++ app/Enums/Railway/Core/MvmTypeAmountEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums\Railway\Core;
+
+enum MvmTypeAmountEnum: string
+{
+    case CHARGE = "charge";
+    case REVENUE = "revenue";
+}
---
+++ app/Models/User/Railway/UserRailwayMouvement.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\User\Railway;
+
+use App\Enums\Railway\Core\MvmTypeAmountEnum;
+use App\Enums\Railway\Core\MvmTypeMvmEnum;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserRailwayMouvement extends Model
+{
+    public $timestamps = false;
+    protected $guarded = [];
+
+    protected $casts = [
+        'type_amount' => MvmTypeAmountEnum::class,
+        'type_mvm' => MvmTypeMvmEnum::class,
+    ];
+
+    public function userRailwayCompany(): BelongsTo
+    {
+        return $this->belongsTo(UserRailwayCompany::class);
+    }
+}
