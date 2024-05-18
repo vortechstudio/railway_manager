900f4f2d539839ab0f144081fc99d266c78ddb13
Vortechstudio <contact@vortechstudio.fr>
2024-05-18 03:31:34 +0200

    chore(Railway Core): Ajout du modèle MessageReward et du typage associé
    - Création du modèle MessageReward
    - Mise en place de la migration pour la table message_rewards
    - Ajout de l'énumération MessageRewardTypeEnum pour le typage du champ reward_type dans le modèle MessageReward

---
+++ app/Models/Railway/Core/MessageReward.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Railway\Core;
+
+use App\Enums\Config\MessageRewardTypeEnum;
+use Illuminate\Database\Eloquent\Model;
+
+class MessageReward extends Model
+{
+    protected $connection = 'mysql';
+    public $timestamps = false;
+    protected $guarded = [];
+    protected $casts = [
+        'reward_type' => MessageRewardTypeEnum::class,
+    ];
+
+    public function message()
+    {
+        return $this->belongsTo(Message::class, 'message_id');
+    }
+}
