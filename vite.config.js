4dc3c1a33a29c47be7154cb7d5cc816f7b2712dc
Vortechstudio <contact@vortechstudio.fr>
2024-05-20 03:46:15 +0200

    syle(General): Transfert du code CSS anime.css vers app.css
    - Suppression du fichier anime.css
    - Ajout du contenu de anime.css à app.css
    - Mise à jour des délais d'animation CSS
    - Ajout de nombreuses classes CSS pour la couleur du texte, l'arrière-plan et la couleur de la bordure

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/login-effect.js', 'resources/sass/animate.scss'],
+            input: [
+                'resources/css/app.css',
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+                'resources/js/login-effect.js',
+            ],
             refresh: true,
         }),
     ],
