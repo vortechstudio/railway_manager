a3bcd84a5f28707c64224efb186f762dbb0962a4
Vortechstudio <contact@vortechstudio.fr>
2024-05-27 19:53:14 +0200

    feat(Network): Ajout du tableau des incidents

---
+++ app/Livewire/Game/Engine/IncidentList.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Livewire\Game\Engine;
+
+
+use App\Models\Railway\Planning\RailwayPlanning;
+use Livewire\Component;
+
+class IncidentList extends Component
+{
+    public $type = '';
+    public $showing = 'simple';
+    public RailwayPlanning $planning;
+    public $incidents;
+
+    public function mount()
+    {
+        $this->incidents = match ($this->type) {
+            default => auth()->user()->railway_incidents()->with('userRailwayHub', 'userRailwayEngine', 'railwayPlanning')->get(),
+            'planning' => $this->planning->incidents()->with('userRailwayHub', 'userRailwayEngine')->get(),
+        };
+    }
+
+    public function render()
+    {
+        return view('livewire.game.engine.incident-list');
+    }
+}
