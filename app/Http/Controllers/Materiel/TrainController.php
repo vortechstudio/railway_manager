90995e2cf22e049c96c8d12e4209ac69e67b9588
Vortechstudio <contact@vortechstudio.fr>
2024-05-31 21:24:47 +0200

    fix(Materiel): Ajout de vérifications d'achat de HUB
    - Ajout d'une vérification du HUB actif lors de l'achat et de la location de trains.

--- app/Http/Controllers/Materiel/TrainController.php
+++ app/Http/Controllers/Materiel/TrainController.php
@@ -13,11 +13,19 @@ class TrainController extends Controller
 
     public function checkout()
     {
+        if(auth()->user()->userRailwayHub()->where('active', true)->count() == 0) {
+            toastr()->addError("Veuillez d'abord acheter un HUB !");
+            return redirect()->route('train.buy');
+        }
         return view('games.materiel.train.checkout');
     }
 
     public function rental()
     {
+        if(auth()->user()->userRailwayHub()->where('active', true)->count() == 0) {
+            toastr()->addError("Veuillez d'abord acheter un HUB !");
+            return redirect()->route('train.buy');
+        }
         return view('games.materiel.train.rental');
     }
 }
