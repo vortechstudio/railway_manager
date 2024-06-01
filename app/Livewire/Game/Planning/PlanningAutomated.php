8bd93721ce2c5f957c0237eebf0c480ba04efe3e
Vortechstudio <contact@vortechstudio.fr>
2024-06-01 04:08:21 +0200

    feat(PlanningAutomated): Ajout de l'automatisation du planning
    - Création du composant Livewire pour l'automatisation du planning
    - Ajout du code de revue au Makefile
    - Mise à jour du .gitignore pour inclure ollama_review.txt
    - Modification de la vue de planning pour intégrer l'automatisation

---
+++ app/Livewire/Game/Planning/PlanningAutomated.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Livewire\Game\Planning;
+
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\Component;
+
+class PlanningAutomated extends Component
+{
+    use LivewireAlert;
+    public bool $automated_planning;
+
+    public function mount()
+    {
+        $this->automated_planning = auth()->user()->railway->automated_planning;
+    }
+
+    public function updatedAutomatedPlanning()
+    {
+        auth()->user()->railway()->update([
+            'automated_planning' => $this->automated_planning
+        ]);
+
+        $this->alert('success', "Configuration changer !");
+        $this->redirectRoute('network.planning.editing');
+    }
+
+    public function render()
+    {
+        return view('livewire.game.planning.planning-automated');
+    }
+}
---
+++ resources/views/livewire/game/planning/planning-automated.blade.php
@@ -0,0 +1,13 @@
+<div class="card shadow-sm">
+    <div class="card-header">
+        <h3 class="card-title">Automatisation du planning</h3>
+    </div>
+    <div class="card-body">
+        <div class="form-check form-switch form-check-custom form-check-solid">
+            <input class="form-check-input" wire:model.live="automated_planning" type="checkbox" value="true" id="flexSwitchDefault"/>
+            <label class="form-check-label" for="flexSwitchDefault">
+                Automatisation
+            </label>
+        </div>
+    </div>
+</div>
