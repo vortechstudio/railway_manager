f02485ea787186662bab9a880ad47ca41a46e1e8
Vortechstudio <contact@vortechstudio.fr>
2024-06-03 09:19:01 +0200

    fix(General): Ajout de la condition pour gérer l'absence de station initiale

--- resources/views/livewire/core/user-planning-progress.blade.php
+++ resources/views/livewire/core/user-planning-progress.blade.php
@@ -1,5 +1,5 @@
 @if(count($plannings) >= 0)
-    <div class="app-navbar-item ms-1 ms-lg-5">
+    <div class="app-navbar-item ms-1 ms-lg-5" wire:poll.visible>
         <div class="btn btn-icon btn-custom btn-color-gray-600 btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px position-relative pulse pulse-danger" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom">
             <i class="ki-duotone ki-route fs-1">
                 <span class="path1"></span>
@@ -34,7 +34,14 @@
                                             </div>
                                             <div class="d-flex flex-column">
                                                 <span class="fw-bold">{{ $planning->userRailwayLigne->railwayLigne->name }}</span>
-                                                <span><strong>Prochain arret:</strong> {{ $planning->stations()->where('status', 'init')->first()->railwayLigneStation->gare->name }}</span>
+                                                <span>
+                                                    <strong>Prochain arret:</strong>
+                                                    @if($planning->stations()->where('status', 'init')->first())
+                                                    {{ $planning->stations()->where('status', 'init')->first()->railwayLigneStation->gare->name }}
+                                                    @else
+                                                    {{ $planning->userRailwayLigne->railwayLigne->end->name }}
+                                                    @endif
+                                                </span>
                                             </div>
                                         </div>
                                         <span class="badge badge-secondary">Arrivée {{ $planning->date_arrived->diffForHumans() }}</span>
