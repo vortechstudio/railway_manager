f6223f5e26a0b498aeb18bf782096452eb20fdd6
Vortechstudio <contact@vortechstudio.fr>
2024-06-05 02:53:50 +0200

    feat(Materiel): Ajout de la gestion des incidents par moteur et intégration de l'onglet des incidents dans la vue des trains

--- resources/views/games/materiel/train/show.blade.php
+++ resources/views/games/materiel/train/show.blade.php
@@ -76,6 +76,9 @@
                 <div class="tab-pane fade" id="trajets" role="tabpanel">
                     @livewire('game.planning.planning-list-by-date', ['type' => 'engine', 'engine' => $engine])
                 </div>
+                <div class="tab-pane fade" id="incidents" role="tabpanel">
+                    @livewire('game.engine.incident-list', ['type' => 'engine', 'engine' => $engine])
+                </div>
             </div>
         </div>
     </div>
