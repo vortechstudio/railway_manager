42cb6a5fefd860f6a2ef182ff85fefd7a250e173
Vortechstudio <contact@vortechstudio.fr>
2024-06-08 20:25:40 +0200

    feat(Network Hub): Remplacement de livewire par leaflet pour la carte des hubs

--- resources/views/games/network/hub/buy.blade.php
+++ resources/views/games/network/hub/buy.blade.php
@@ -25,11 +25,48 @@
             </div>
             <div class="card shadow-sm my-5">
                 <div class="card-body">
-                    @livewire('game.core.map', ["type" => "hubs", "hubs" => $hubs])
+                    <div id="mapHubs" style="width: 100%; height: 450px;"></div>
                 </div>
             </div>
             @livewire('game.network.hub-checkout', ['hubs' => $hubs])
         </div>
     </div>
 @endsection
+
+@push('styles')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <link rel="stylesheet" href="{{ asset('/plugins/custom/leaflet-routing-machine/dist/leaflet-routing-machine.css') }}">
+@endpush
+
+@push('scripts')
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+            crossorigin=""></script>
+    <script src="{{ asset('/plugins/custom/leaflet-routing-machine/dist/leaflet-routing-machine.js') }}"></script>
+
+    <script type="text/javascript">
+        let mapHubs = L.map('mapHubs').setView(['46.762443', '2.974984'], 6)
+        let iconHubBuying = L.icon({
+            iconUrl: '{{ Storage::url('icons/railway/map_buy.png') }}',
+            iconSize: [30, 30],
+        })
+        let iconHub = L.icon({
+            iconUrl: '{{ Storage::url('icons/railway/map_not_buy.png') }}',
+            iconSize: [30, 30],
+        })
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(mapHubs);
+
+        @foreach(\App\Models\Railway\Gare\RailwayHub::where('active', true)->get() as $hub)
+            @if(!auth()->user()->userRailwayHub()->where('railway_hub_id', $hub->id)->exists())
+                L.marker([{{ $hub->gare->latitude }}, {{ $hub->gare->longitude }}], {icon: iconHub}).addTo(mapHubs)
+            @else
+                L.marker([{{ $hub->gare->latitude }}, {{ $hub->gare->longitude }}], {icon: iconHubBuying}).addTo(mapHubs)
+           @endif
+        @endforeach
+    </script>
+@endpush
 
