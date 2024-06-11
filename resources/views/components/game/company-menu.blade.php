f18669711d2de516dde69cb12f3b1e4c169f63b6
Vortechstudio <contact@vortechstudio.fr>
2024-06-11 07:23:32 +0200

    feat(Compagnie): Ajout vue de la compagnie et fonctionnalités associées
    - Ajout de la vue de statistiques de la compagnie
    - Simplification du processus de maintenance des locomotives

--- resources/views/components/game/company-menu.blade.php
+++ resources/views/components/game/company-menu.blade.php
@@ -9,8 +9,8 @@
             <img src="{{ Storage::url('icons/railway/ranking.png') }}" alt="">
         </span>
     </a>
-    <a href="{{ route('train.index') }}" class="btn btn-flush">
-        <span class="symbol symbol-40px symbol-circle bg-active-primary @if(route_is(['train.index'])) active @endif" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Compagnie">
+    <a href="{{ route('company.index') }}" class="btn btn-flush">
+        <span class="symbol symbol-40px symbol-circle bg-active-primary @if(route_is(['company.index'])) active @endif" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Compagnie">
             <img src="{{ Storage::url('icons/railway/hq.png') }}" alt="">
         </span>
     </a>
