5dee0f1482d993187279cb466bcb44d8f5b394ba
Vortechstudio <contact@vortechstudio.fr>
2024-07-01 20:26:44 +0200

    fix(Network): Ajout de carte et déplacement de scripts mapLigne

--- resources/views/livewire/game/network/ligne-list.blade.php
+++ resources/views/livewire/game/network/ligne-list.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div id="mapLigne" class="mb-5" style="height: 500px;"></div>
+
     @if(count($lignes) == 0)
         <x-base.is-null
             text="Aucune Ligne active actuellement"/>
@@ -50,91 +50,3 @@
         @endforeach
     @endif
 </div>
-
-@push("styles")
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossorigin=""/>
-@endpush
-
-@push('scripts')
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-            crossorigin=""></script>
-@endpush
-
-@push("scripts")
-    <script>
-        let mapLigne = L.map('mapLigne').setView([{{ $hub->railwayHub->gare->latitude }}, {{ $hub->railwayHub->gare->longitude }}], 10)
-        let iconHub = L.icon({
-            iconUrl: '{{ Storage::url('icons/railway/station_hub.png') }}',
-            iconSize: [30, 30],
-        });
-        let iconStation = L.icon({
-            iconUrl: '{{ Storage::url('icons/railway/station.png') }}',
-            iconSize: [30, 30],
-        });
-        let iconEnd = L.icon({
-            iconUrl: '{{ Storage::url('icons/railway/station_end.png') }}',
-            iconSize: [30, 30],
-        });
-        let hub = @json($hub)
-
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(mapLigne);
-
-        let markerHubLigne = L.marker([{{ $hub->railwayHub->gare->latitude }}, {{ $hub->railwayHub->gare->longitude }}], {icon: iconHub}).addTo(mapLigne)
-
-        markerHubLigne.bindPopup(`
-                <div class="d-flex flex-column">
-                    <div class="d-flex flex-row align-items-center gap-3 mb-3">
-                        <div class="symbol symbol-30px">
-                            <img src="{{ Storage::url('icons/railway/station.png') }}" alt="">
-                        </div>
-                        <span class="text-danger fw-bold">${hub.railway_hub.gare.name}</span>
-                    </div>
-                    @livewire('game.core.screen-departure')
-                </div>
-        `)
-
-        hub.user_railway_ligne.forEach(ligne => {
-            console.log(ligne)
-            let marker = L.marker([ligne.railway_ligne.end.latitude, ligne.railway_ligne.end.longitude], {icon: iconEnd}).addTo(mapLigne)
-            marker.bindPopup(`
-                <div class="d-flex flex-column">
-                    <div class="d-flex flex-row align-items-center gap-3 mb-3">
-                        <div class="symbol symbol-30px">
-                            <img src="{{ Storage::url('icons/railway/station_end.png') }}" alt="">
-                        </div>
-                        <span class="text-danger fw-bold">Ligne ${ligne.railway_ligne.start.name} <-> ${ligne.railway_ligne.end.name}</span>
-                    </div>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <span>Prochain départ</span>
-                        <span class="fw-bold">${ligne.next_departure !== null ? ligne.next_departure.date_depart : 'Aucun Départ'}</span>
-                    </div>
-                </div>
-                `)
-            let latLngPolyline = []
-            let lineTypeColor = {
-                'ter': '#034EA2',
-                'tgv': '#9b2743',
-                'intercity': '#7B1791',
-                'tram': 'black',
-                'transilien': '#83BF00',
-                'metro': 'black',
-                'other': 'black',
-            }
-
-            ligne.railway_ligne.stations.forEach(station => {
-                latLngPolyline.push([station.gare.latitude, station.gare.longitude])
-            })
-
-            L.polyline(latLngPolyline, {
-                color: lineTypeColor[ligne.railway_ligne.type] || 'black',
-                weight: 5,
-            }).addTo(mapLigne)
-        })
-    </script>
-@endpush
